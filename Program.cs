73cb4f2986bbe1f4e7ad44363f9c36d2370170f6
waichungo <waichungojames@gmail.com>
2024-03-20 01:00:05 +0300

    Added last input getter

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ using System.IO.Compression;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,10 +23,37 @@ namespace UiAutomate
         public int width;
         public int height;
     }
+    internal struct LASTINPUTINFO
+    {
+        public uint cbSize;
+
+        public uint dwTime;
+    }
+
     public class Program
     {
+        const uint SWP_NOSIZE = 0x0001;
+        const uint SWP_NOZORDER = 0x0004;
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         public static extern int GetSystemMetrics(int nIndex);
+        [DllImport("user32.dll")]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        [DllImport("user32.dll")]
+        public static extern int SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        [DllImport("User32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        [DllImport("Kernel32.dll")]
+        private static extern uint GetLastError();
+
+        public static uint GetIdleTime()
+        {
+            LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+            GetLastInputInfo(ref lastInPut);
+
+            return ((uint)Environment.TickCount - lastInPut.dwTime);
+        }
         static Rect GetScreenRect()
         {
             int width = GetSystemMetrics(0);
@@ -72,9 +101,29 @@ namespace UiAutomate
 
 
         }
+        public static long GetLastInput()
+        {
+            long res = 0L;
+            LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+            if (GetLastInputInfo(ref lastInPut))
+            {
+                res = (Environment.TickCount - lastInPut.dwTime) / 1000;
+            }
+            return res;
+        }
         public static string GetHoneygainPath()
         {
             var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "honeygain");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            return path;
+        }
+        public static string GetDummyWinPath()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dummyWin");
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
@@ -110,11 +159,41 @@ namespace UiAutomate
             }
             return true;
         }
+        public static bool VerifyDummyWinExtraction()
+        {
+            using (var stream = new MemoryStream(Resources.dummywin))
+            {
+                using (ZipArchive archive = new ZipArchive(stream))
+                {
+                    var HgPath = GetDummyWinPath();
+                    foreach (var item in archive.Entries)
+                    {
+                        var file = Path.Combine(HgPath, item.FullName.TrimEnd('/'));
+                        if (item.Length > 0)
+                        {
+                            if (!File.Exists(file) || new FileInfo(file).Length != item.Length)
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            if (!Directory.Exists(file))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
         public static void ExtractHG()
         {
             while (!VerifyHoneygainExtraction())
             {
                 var path = GetHoneygainPath();
+                Directory.Delete(path, true);
                 using (var stream = new MemoryStream(Resources.Honeygain))
                 {
                     using (ZipArchive archive = new ZipArchive(stream))
@@ -128,29 +207,78 @@ namespace UiAutomate
 
 
         }
+        public static void ExtractDummyWin()
+        {
+            while (!VerifyDummyWinExtraction())
+            {
+                var path = GetDummyWinPath();
+                Directory.Delete(path, true);
+                using (var stream = new MemoryStream(Resources.dummywin))
+                {
+                    using (ZipArchive archive = new ZipArchive(stream))
+                    {
+                        archive.ExtractToDirectory(path);
+                    }
+                }
+            }
+        }
+        public static Process LaunchDummyWin()
+        {
+            var pr = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = Path.Combine(GetDummyWinPath(), "dummywin.exe"),
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden,                    
+                }
+            };
+            pr.Start();
+            //pr.WaitForExit();
+            return pr;
+        }
         public static bool Start()
         {
             var succeeded = false;
             WaitForConnection();
-            ExtractHG();
+            KillDummyWinProcesses();
             KillHoneyGainProcesses();
+            ExtractDummyWin();
+            ExtractHG();
+            var dummyWin = LaunchDummyWin();
+            var rect = GetScreenRect();
+            SetWindowPos(dummyWin.MainWindowHandle, IntPtr.Zero, rect.width + 100, rect.height + 100, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
             var app = GetApplication();
+            bool running = true;
+
+            Thread th = null;
+
             using (var automation = new UIA3Automation())
             {
                 var window = app.GetMainWindow(automation);
-                var rect = GetScreenRect();
+                
+                th = new Thread(() =>
+               {
+                   while (running)
+                   {
+                       Thread.Sleep(200);
+                       Console.WriteLine(GetLastInput());
+                       SetWindowPos(dummyWin.MainWindowHandle, IntPtr.Zero, rect.width + 100, rect.height + 100, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
+                       //SetWindowPos(app.MainWindowHandle, IntPtr.Zero, rect.width + 100, rect.height + 100, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
+                       //foreach(var dialog in window.ModalWindows)
+                       //    {
+                       //        SetWindowPos(dialog.Ma, IntPtr.Zero, rect.width + 100, rect.height + 100, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
+                       //    }
+                   }
+
+               });
+                th.Start();
+               
                 if (rect.width > 0)
                 {
-                    if (!window.IsOffscreen)
-                    {
-                        try
-                        {
-                            window.Move(rect.width + 2, rect.height + 2);
-                        }
-                        catch { }
-                    }
+                    //SetWindowPos(app.MainWindowHandle, IntPtr.Zero, rect.width+100, rect.height+100, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
                 }
-
                 while (IsWindowLoading(window))
                 {
                     Thread.Sleep(1000);
@@ -234,8 +362,14 @@ namespace UiAutomate
                 else
                 {
                     succeeded = true;
+                    SetParent(app.MainWindowHandle, dummyWin.MainWindowHandle);
                 }
-
+                running = false;
+                th.Join();
+                if (succeeded)
+                {
+                    SetParent(app.MainWindowHandle, dummyWin.MainWindowHandle);
+                }
 
             }
             return succeeded;
@@ -263,6 +397,23 @@ namespace UiAutomate
                try
                {
                    return pr.MainModule.FileName.ToLower().EndsWith("honeygain.exe");
+               }
+               catch { }
+               return false;
+           }).ToList().ForEach(e =>
+           {
+               KillProcess(e.Id);
+           });
+
+
+        }
+        static void KillDummyWinProcesses()
+        {
+            Process.GetProcesses().Where(pr =>
+           {
+               try
+               {
+                   return pr.MainModule.FileName.EndsWith("dummywin.exe");
                }
                catch { }
                return false;
